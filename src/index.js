c08cad4607365acc90ff42f568ae8b18db254466
Maiev <elena.kalichkina.rho@gmail.com>
2021-03-15 03:36:44 +0700

    feat: add solve

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 module.exports = function check(str, bracketsConfig) {
   // your solution
-}
+  let stack = [];
+
+  for (sign of str) {
+      for (arr of bracketsConfig) {
+      if (sign == arr[0] && sign == arr[1] && sign == stack[stack.length - 1]) { 
+          stack.pop();
+      } else if (sign == arr[0]) {
+          stack.push(sign);
+      } else if (sign == arr[1] && stack[stack.length - 1] == arr[0]) {
+          stack.pop();
+      } else if (sign == arr[1] && stack[stack.length - 1] != arr[0]) {stack.push(sign);}
+      }         
+  }        
+  console.log(stack);
+  
+  if (stack.length == 0) {
+      return true;
+  } else {
+      return false;
+  }
+  }
+  
